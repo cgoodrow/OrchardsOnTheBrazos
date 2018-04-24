1fa0a497001560b9a4d399fd0e7187b13dd6325e
Chris Goodrow <goodrow.chris4@gmail.com>
2018-04-24 18:59:16 -0500

    Add project files.

---
+++ OrchardsOnTheBrazos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OrchardsOnTheBrazos.MvcApplication" Language="C#" %>
---
+++ OrchardsOnTheBrazos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrchardsOnTheBrazos.Startup))]
+namespace OrchardsOnTheBrazos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
